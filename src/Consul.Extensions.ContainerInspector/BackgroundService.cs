d13d26f41af092b0c4f157d21f4a03e51f26003d
syneder <67856698+syneder@users.noreply.github.com>
2024-07-05 14:26:08 +0300

    Added entry point and BackgroundService

---
+++ src/Consul.Extensions.ContainerInspector/BackgroundService.cs
@@ -0,0 +1,26 @@
+﻿
+using Consul.Extensions.ContainerInspector.Configuration.Models;
+using Microsoft.Extensions.Options;
+
+namespace Consul.Extensions.ContainerInspector
+{
+    public class BackgroundService : Microsoft.Extensions.Hosting.BackgroundService
+    {
+        private readonly ConsulConfiguration _configuration;
+        private readonly ManagedInstanceRegistration _instanceRegistration;
+
+        public BackgroundService(
+            IOptions<ConsulConfiguration> configuration,
+            IOptions<ManagedInstanceRegistration> instanceRegistration)
+        {
+            _configuration = configuration?.Value ?? throw new ArgumentNullException(nameof(configuration));
+            _instanceRegistration = instanceRegistration?.Value
+                ?? throw new ArgumentNullException(nameof(instanceRegistration));
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Consul.Extensions.ContainerInspector/Program.cs
@@ -0,0 +1,62 @@
+﻿using Consul.Extensions.ContainerInspector.Configuration.Models;
+using Consul.Extensions.ContainerInspector.Extensions;
+
+namespace Consul.Extensions.ContainerInspector
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+        {
+            var appBuilder = new HostBuilder()
+                .UseServiceProviderFactory(_ => new DefaultServiceProviderFactory())
+                .ConfigureServices(ConfigureServices)
+                .ConfigureServices(services =>
+                {
+                    services.AddLogging(serviceLogging => ConfigureLogging(serviceLogging, args));
+                })
+                .ConfigureAppConfiguration((_, configurationBuilder) =>
+                {
+                    configurationBuilder.AddCommandLine(args);
+
+                    configurationBuilder.AddConsulConfiguration();
+                    configurationBuilder.AddManagedInstanceRegistration();
+                });
+
+            return appBuilder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.Configure<ConsulConfiguration>(
+                context.Configuration.GetSection(
+                    Extensions.ConfigurationExtensions.ConsulConfigurationSection));
+
+            services.Configure<ManagedInstanceRegistration>(
+                context.Configuration.GetSection(
+                    Extensions.ConfigurationExtensions.ManagedInstanceConfigurationSection));
+
+            services.AddHostedService<BackgroundService>();
+        }
+
+        private static void ConfigureLogging(ILoggingBuilder serviceLogging, string[] args)
+        {
+            if (args.Contains("--debug", StringComparer.OrdinalIgnoreCase))
+            {
+                serviceLogging.SetMinimumLevel(LogLevel.Debug);
+            }
+
+            serviceLogging.AddSystemdConsole(options =>
+            {
+                options.UseUtcTimestamp = true;
+            });
+
+            serviceLogging.Configure(options =>
+            {
+                options.ActivityTrackingOptions =
+                    ActivityTrackingOptions.SpanId |
+                    ActivityTrackingOptions.TraceId |
+                    ActivityTrackingOptions.ParentId;
+            });
+        }
+    }
+}
